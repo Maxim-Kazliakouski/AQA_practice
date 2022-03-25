dfb4fabdafce0ebc92761767a1bae59357dab2d7
Maxim Kazliakouski <max_kazliakouski@MacBook-Pro-Maxim.local>
2022-03-26 01:25:21 +0300

    adding new tests and data, conftest file

---
+++ Tests/tests_FormsPage/data_FormsPage.py
@@ -0,0 +1,10 @@
+class TestDataFormsPage:
+    # urls
+    FORMS_PAGE_URL = 'https://demoqa.com/automation-practice-form'
+
+    # data for form
+    FIRST_NAME = 'Max'
+    LAST_NAME = 'Kazliakouski'
+    EMAIL = '123@mail.com'
+
+
---
+++ Tests/tests_FormsPage/test_FormsPage.py
@@ -0,0 +1,42 @@
+import time
+
+import pytest
+from Locators.FormsPage_locators import FormsPageLocators
+from Locators.MainPage_locators import MainPageLocators
+from Pages.FormsPage import FormsPage
+from Tests.tests_FormsPage.data_FormsPage import TestDataFormsPage
+from Tests.tests_MainPage.data_MainPage import TestDataMainPage
+from selenium.webdriver.common.by import By
+
+
+class Test_FormsPage:
+    @pytest.mark.FormsSection
+    class Test_Forms:
+        class Test_positive:
+            def test_user_on_the_forms_page(self, browser, logs_forms_page):
+                link = TestDataMainPage.MAIN_PAGE_URL
+                page = FormsPage(browser, link)
+                page.open_page(link)
+                page.removing_advertisement()
+                page.click_on_element(MainPageLocators.FORMS_BUTTON)
+                page.go_to_section(FormsPageLocators.PRACTICE_FORMS)
+                forms_page_url = page.getting_current_url()
+                try:
+                    assert forms_page_url == TestDataFormsPage.FORMS_PAGE_URL,\
+                        "User isn't on Forms Page"
+                except AssertionError as err:
+                    logs_forms_page.error("User isn't on Forms Page")
+                    raise err
+
+            def test_filling_form(self, browser, logs_forms_page):
+                link = TestDataFormsPage.FORMS_PAGE_URL
+                page = FormsPage(browser, link)
+                page.open_page(link)
+                page.removing_advertisement()
+                page.filling_form()
+                modal_window = page.search_element(FormsPageLocators.MODAL_WINDOW)
+                try:
+                    assert modal_window, "There is no modal window after clicking on submit button"
+                except AssertionError as err:
+                    logs_forms_page.error("There is no modal window after clicking on submit button")
+                    raise err
