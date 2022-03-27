9335540630c07f39fe8e6c5af358b28cf1dfb110
Maxim Kazliakouski <max_kazliakouski@MacBook-Pro-Maxim.local>
2022-03-27 12:56:50 +0300

    adding new data for tests

--- Tests/tests_AlertsFrameWindows/data_AlertsFrameWindows.py
+++ Tests/tests_AlertsFrameWindows/data_AlertsFrameWindows.py
@@ -2,6 +2,8 @@ class TestDataAlertsFrameWindows:
     # urls
     BROWSER_WINDOWS_URL = 'https://demoqa.com/browser-windows'
     ALERTS_URL = 'https://demoqa.com/alerts'
+    FRAMES_URL = 'https://demoqa.com/frames'
+    MODAL_WINDOW_URL = 'https://demoqa.com/modal-dialogs'
     # for windows browser section
     INFO_NEW_TAB = 'This is a sample page'
     # for alerts section
@@ -10,6 +12,17 @@ class TestDataAlertsFrameWindows:
     CONFIRM_ALERT_RESULT_OK = 'You selected Ok'
     CONFIRM_ALERT_RESULT_CANCEL = 'You selected Cancel'
     TEXT_FOR_ALERT = 'Max Kazliakouski'
+
+    # for iframes section
+    TEXT_FIRST_IFRAME = 'This is a sample page'
+    TEXT_SECOND_IFRAME = 'This is a sample page'
+
+    # for modal dialogs
+    TEXT_SMALL_MODAL = 'This is a small modal. It has very less content'
+    CLOSE_BUTTONS_NAMES = ['CLOSE_BUTTON', 'CLOSE_X_BUTTON']
+
+
+
 
 
 
