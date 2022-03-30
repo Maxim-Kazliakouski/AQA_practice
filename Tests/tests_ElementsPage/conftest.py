e6d27e410ec9eb9845713ee7f24d74ee88832b86
Maxim Kazliakouski <max_kazliakouski@MacBook-Pro-Maxim.local>
2022-03-30 21:26:46 +0300

    adding new changes for conftest

--- Tests/tests_ElementsPage/conftest.py
+++ Tests/tests_ElementsPage/conftest.py
@@ -2,14 +2,12 @@ import time
 import pytest
 import logging
 import subprocess
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 import os
 from Locators.ElementsPage_locators import ElementPageLocators
 from Pages.ElementsPage import ElementsPage
 from Tests.tests_ElementsPage.data_ElementsPage import TestDataElementsPage
 from Tests.tests_MainPage.data_MainPage import TestDataMainPage
+from Tests.tests_MainPage.conftest import browser
 
 
 @pytest.fixture(scope='function')
@@ -33,7 +31,7 @@ def logs_elements_page():
 
 
 @pytest.fixture(scope='function')
-def adding_email():
+def adding_email(browser):
     link = TestDataMainPage.ELEMENTS_PAGE_URL
     page = ElementsPage(browser, link)
     page.open_page(link)
@@ -46,7 +44,7 @@ def adding_email():
 
 
 @pytest.fixture(scope='function')
-def adding_fullname():
+def adding_fullname(browser):
     link = TestDataMainPage.ELEMENTS_PAGE_URL
     page = ElementsPage(browser, link)
     page.open_page(link)
@@ -59,7 +57,7 @@ def adding_fullname():
 
 
 @pytest.fixture(scope='function')
-def adding_current_address():
+def adding_current_address(browser):
     link = TestDataMainPage.ELEMENTS_PAGE_URL
     page = ElementsPage(browser, link)
     page.open_page(link)
@@ -72,7 +70,7 @@ def adding_current_address():
 
 
 @pytest.fixture(scope='function')
-def adding_permanent_address():
+def adding_permanent_address(browser):
     link = TestDataMainPage.ELEMENTS_PAGE_URL
     page = ElementsPage(browser, link)
     page.open_page(link)
@@ -85,7 +83,7 @@ def adding_permanent_address():
 
 
 @pytest.fixture(scope='function')
-def enable_checkbox_home():
+def enable_checkbox_home(browser):
     link = TestDataElementsPage.CHECK_BOX_URL
     page = ElementsPage(browser, link)
     page.open_page(link)
@@ -105,126 +103,8 @@ def removing_file_after_downloading():
     subprocess.run((['rm', '/Users/max_kazliakouski/Downloads/sampleFile.jpeg']))
 
 
-def pytest_addoption(parser):
-    parser.addoption('--browser.name', action='store', default='chrome',
-                     help="Choose browser: chrome or safari")
-    parser.addoption('--headmode', action='store', default='true',
-                     help='Choose turn on or turn off headless mode')
-    parser.addoption('--language', action='store', default=None,
-                     help='Choose language: ru, en...(etc)')
-
-
 @pytest.fixture(scope='session')
 def clearing_results_folder():
     print('\nClearing results folder...')
     time.sleep(2)
     os.system("rm -rf /Volumes/MacOS/Users/maxkazliakouski/.jenkins/workspace/POM_tests/allure-results/*")
-
-
-@pytest.fixture(scope='class')
-def browser(request):
-    browser_name = request.config.getoption('browser.name')
-    headless = request.config.getoption('headmode')
-    print(f'\nStarting browser {browser_name}...')
-    global browser
-    # user_language = request.config.getoption('language')
-    if browser_name == 'chrome':
-        # here we set in commandline choosing for headless mode
-        if headless == 'true':
-            options = webdriver.ChromeOptions()
-            prefs = {"download.default_directory": "/Users/max_kazliakouski/Downloads/"}
-            # example: prefs = {"download.default_directory" : "C:\Tutorial\down"};
-            options.add_experimental_option("prefs", prefs)
-            # adding browser options!!! important
-            options.add_argument(
-                "user-data-dir=/Users/max_kazliakouski/Library/Application Support/Google/Chrome/Default")
-            # prefs = {"profile.default_content_setting_values.notifications": 2}
-            # options.add_experimental_option("prefs", prefs)
-            # options.add_argument("--disable-notifications")
-            options.add_argument(
-                "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
-            options.headless = True
-            s = Service('Tools/chromedriver')
-            browser = webdriver.Chrome(service=s, options=options)
-            # params for docker
-            options = webdriver.ChromeOptions()
-            options.add_argument('--no-sandbox')
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            # s = Service('/usr/local/bin/chromedriver')
-            # browser = webdriver.Chrome(service=s, options=options)
-            browser.maximize_window()
-            browser.implicitly_wait(5)
-        else:
-            # options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-            options = webdriver.ChromeOptions()
-            options.add_argument(
-                "user-data-dir=/Users/max_kazliakouski/Library/Application Support/Google/Chrome/SeleniumProfile")
-            # prefs = {"profile.default_content_setting_values.notifications": 2}
-            # options.add_experimental_option("prefs", prefs)
-            # options.add_argument(
-            # "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
-            options.headless = False
-            s = Service('Tools/chromedriver')
-            browser = webdriver.Chrome(service=s, options=options)
-            browser.maximize_window()
-            browser.implicitly_wait(5)
-    elif browser_name == 'mozila':
-        if headless == 'true':
-            options = webdriver.FirefoxOptions()
-            options.headless = True
-            s = Service('Tools/geckodriver')
-            browser = webdriver.Firefox(service=s, options=options)
-            # params for docker
-            options = webdriver.FirefoxOptions()
-            options.add_argument('--no-sandbox')
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            browser.maximize_window()
-            browser.implicitly_wait(5)
-        else:
-            options = webdriver.FirefoxOptions()
-            options.headless = False
-            s = Service('Tools/geckodriver')
-            browser = webdriver.Firefox(service=s, options=options)
-            browser.maximize_window()
-            browser.implicitly_wait(5)
-    elif browser_name == 'opera':
-        if headless == 'true':
-            options = webdriver.Opera
-            options.headless = True
-            browser = webdriver.Opera(executable_path='Tools/operadriver')
-            # params for docker
-            # options = webdriver.Opera()
-            # options.add_argument('--no-sandbox')
-            # options.add_argument('--headless')
-            # options.add_argument('--disable-gpu')
-            browser.maximize_window()
-            browser.implicitly_wait(5)
-        else:
-            options = webdriver.Opera()
-            options.headless = False
-            browser = webdriver.Opera(executable_path='Tools/operadriver')
-            browser.maximize_window()
-            browser.implicitly_wait(5)
-    elif browser_name == 'safari':
-        s = Service('Tools/safaridriver')
-        browser = webdriver.Safari(service=s)
-        browser.maximize_window()
-        browser.implicitly_wait(5)
-        print(f'Start {browser_name} browser for test...')
-    else:
-        print(f'Browser {browser_name} still not implemented')
-
-    yield browser
-    print(f'\nQuit browser {browser_name}...')
-    # browser quit don't fit for safari, get the error about refuse connection
-    browser.quit()
-
-    # return os.system('allure serve results')
-
-# def parser(section, value):
-#     config = ConfigParser()
-#     config.read('/Volumes/Work/Parse_project/data_for_test/test_data_hw21.ini')
-#     value = config.get(section, value)
-#     return value
--- Tests/tests_MainPage/conftest.py
+++ Tests/tests_MainPage/conftest.py
@@ -27,13 +27,13 @@ def logs_main_page():
     return logger
 
 
-# def pytest_addoption(parser):
-#     parser.addoption('--browser.name', action='store', default='chrome',
-#                      help="Choose browser: chrome or safari")
-#     parser.addoption('--headmode', action='store', default='true',
-#                      help='Choose turn on or turn off headless mode')
-#     parser.addoption('--language', action='store', default=None,
-#                      help='Choose language: ru, en...(etc)')
+def pytest_addoption(parser):
+    parser.addoption('--browser.name', action='store', default='chrome',
+                     help="Choose browser: chrome or safari")
+    parser.addoption('--headmode', action='store', default='true',
+                     help='Choose turn on or turn off headless mode')
+    parser.addoption('--language', action='store', default=None,
+                     help='Choose language: ru, en...(etc)')
 
 
 @pytest.fixture(scope='session')
@@ -43,7 +43,7 @@ def clearing_results_folder():
     os.system("rm -rf /Volumes/MacOS/Users/maxkazliakouski/.jenkins/workspace/POM_tests/allure-results/*")
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='class')
 def browser(request):
     browser_name = request.config.getoption('browser.name')
     headless = request.config.getoption('headmode')
@@ -54,8 +54,11 @@ def browser(request):
         # here we set in commandline choosing for headless mode
         if headless == 'true':
             options = webdriver.ChromeOptions()
+            prefs = {"download.default_directory": "/Users/max_kazliakouski/Downloads/"}
+            options.add_experimental_option("prefs", prefs)
             # adding browser options!!! important
-            options.add_argument("user-data-dir=/Users/max_kazliakouski/Library/Application Support/Google/Chrome/Default")
+            options.add_argument(
+                "user-data-dir=/Users/max_kazliakouski/Library/Application Support/Google/Chrome/Default")
             # prefs = {"profile.default_content_setting_values.notifications": 2}
             # options.add_experimental_option("prefs", prefs)
             # options.add_argument("--disable-notifications")
@@ -150,3 +153,105 @@ def browser(request):
 #     config.read('/Volumes/Work/Parse_project/data_for_test/test_data_hw21.ini')
 #     value = config.get(section, value)
 #     return value
+
+
+@pytest.fixture(scope='function')
+def browser_xfail(request):
+    browser_name = request.config.getoption('browser.name')
+    # headless = request.config.getoption('headmode')
+    print(f'\nStarting browser {browser_name}...')
+    global browser
+    headless = 'false'
+    # user_language = request.config.getoption('language')
+    if browser_name == 'chrome':
+        # here we set in commandline choosing for headless mode
+        if headless == 'true':
+            options = webdriver.ChromeOptions()
+            prefs = {"download.default_directory": "/Users/max_kazliakouski/Downloads/"}
+            # example: prefs = {"download.default_directory" : "C:\Tutorial\down"};
+            options.add_experimental_option("prefs", prefs)
+            # adding browser options!!! important
+            options.add_argument(
+                "user-data-dir=/Users/max_kazliakouski/Library/Application Support/Google/Chrome/Default")
+            # prefs = {"profile.default_content_setting_values.notifications": 2}
+            # options.add_experimental_option("prefs", prefs)
+            # options.add_argument("--disable-notifications")
+            options.add_argument(
+                "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
+            options.headless = True
+            s = Service('Tools/chromedriver')
+            browser = webdriver.Chrome(service=s, options=options)
+            # params for docker
+            options = webdriver.ChromeOptions()
+            options.add_argument('--no-sandbox')
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            # s = Service('/usr/local/bin/chromedriver')
+            # browser = webdriver.Chrome(service=s, options=options)
+            browser.maximize_window()
+            browser.implicitly_wait(5)
+        else:
+            # options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+            options = webdriver.ChromeOptions()
+            options.add_argument(
+                "user-data-dir=/Users/max_kazliakouski/Library/Application Support/Google/Chrome/SeleniumProfile")
+            # prefs = {"profile.default_content_setting_values.notifications": 2}
+            # options.add_experimental_option("prefs", prefs)
+            # options.add_argument(
+            # "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
+            options.headless = False
+            s = Service('Tools/chromedriver')
+            browser = webdriver.Chrome(service=s, options=options)
+            browser.maximize_window()
+            browser.implicitly_wait(5)
+    elif browser_name == 'mozila':
+        if headless == 'true':
+            options = webdriver.FirefoxOptions()
+            options.headless = True
+            s = Service('Tools/geckodriver')
+            browser = webdriver.Firefox(service=s, options=options)
+            # params for docker
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--no-sandbox')
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            browser.maximize_window()
+            browser.implicitly_wait(5)
+        else:
+            options = webdriver.FirefoxOptions()
+            options.headless = False
+            s = Service('Tools/geckodriver')
+            browser = webdriver.Firefox(service=s, options=options)
+            browser.maximize_window()
+            browser.implicitly_wait(5)
+    elif browser_name == 'opera':
+        if headless == 'true':
+            options = webdriver.Opera
+            options.headless = True
+            browser = webdriver.Opera(executable_path='Tools/operadriver')
+            # params for docker
+            # options = webdriver.Opera()
+            # options.add_argument('--no-sandbox')
+            # options.add_argument('--headless')
+            # options.add_argument('--disable-gpu')
+            browser.maximize_window()
+            browser.implicitly_wait(5)
+        else:
+            options = webdriver.Opera()
+            options.headless = False
+            browser = webdriver.Opera(executable_path='Tools/operadriver')
+            browser.maximize_window()
+            browser.implicitly_wait(5)
+    elif browser_name == 'safari':
+        s = Service('Tools/safaridriver')
+        browser = webdriver.Safari(service=s)
+        browser.maximize_window()
+        browser.implicitly_wait(5)
+        print(f'Start {browser_name} browser for test...')
+    else:
+        print(f'Browser {browser_name} still not implemented')
+
+    yield browser
+    print(f'\nQuit browser {browser_name}...')
+    # browser quit don't fit for safari, get the error about refuse connection
+    browser.quit()
