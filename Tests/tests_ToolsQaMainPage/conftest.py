009e43ee8f21f57142a3859d5c6e09cc794a38a9
Maxim Kazliakouski <max_kazliakouski@MacBook-Pro-Maxim.local>
2022-04-02 01:00:23 +0300

    adding new fixtures to conftest

---
+++ Tests/tests_ToolsQaMainPage/conftest.py
@@ -0,0 +1,33 @@
+import time
+import pytest
+import logging
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import os
+from Locators.ElementsPage_locators import ElementPageLocators
+from Pages.ElementsPage import ElementsPage
+from Tests.tests_ElementsPage.data_ElementsPage import TestDataElementsPage
+from Tests.tests_MainPage.data_MainPage import TestDataMainPage
+
+
+@pytest.fixture(scope='function')
+def logs_tool_qa_main_page():
+    # to get the name of the test case file name at runtime
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    # FileHandler class to set the location of log file
+    fileHandler = logging.FileHandler('./logfile.log', mode='a', delay=False)
+    # Formatter class to set the format of log file
+    formatter = logging.Formatter(
+        "[%(asctime)s] -- [%(levelname)s][(See error near the line -> %(lineno)d]--[%(name)s->tests_ToolsQaMainPage]: \n%(message)s")
+    # object of FileHandler gets formatting info from setFormatter #method
+    fileHandler.setFormatter(formatter)
+    # logger object gets formatting, path of log file info with addHandler #method
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.addHandler(fileHandler)
+    # setting logging level to INFO
+    return logger
+
