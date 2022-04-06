a20697afc79631035da0276a2bebdd02f8032697
Maxim Kazliakouski <max_kazliakouski@MacBook-Pro-Maxim.local>
2022-04-07 00:41:40 +0300

    adding new methods

--- Pages/ToolsQaMainPage.py
+++ Pages/ToolsQaMainPage.py
@@ -10,5 +10,14 @@ class ToolsQaMainPage(BasePage):
         section = self.browser.find_element(By.XPATH, f"//span[contains(text(),'{name_section}')]")
         action.move_to_element(section).perform()
 
+    def handling_categories_without_start_learning(self, list_for_gen):
+        new_cat_list = []
+        for i in list_for_gen:
+            if i == 'Start Learning':
+                continue
+            else:
+                new_cat_list.append(i)
+        return new_cat_list
+
 
 
